7d53aa9f018f9c45c8d52622ad58b019932c3c17
basireddyrt <dev.remote-teams.in>
2026-04-29 12:19:13 +0530

    next.config

---
+++ next.config.js
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true, // required for static export
+  },
+};
+
+module.exports = nextConfig;
